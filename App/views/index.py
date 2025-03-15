637cddced4fed8549d46edf6f63a15b520150a23
Kwasi Edwards <kwasiedwards@gmail.com>
2025-03-15 17:08:52 +0000

    added a new /hello route

--- App/views/index.py
+++ App/views/index.py
@@ -12,6 +12,10 @@ def init():
     initialize()
     return jsonify(message='db initialized!')
 
+@index_views.route('/hello', methods=['GET'])
+def hello():
+    return jsonify(message='Hello World!')
+
 @index_views.route('/health', methods=['GET'])
 def health_check():
     return jsonify({'status':'healthy'})
